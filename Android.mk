6cc785522776117ab83499770ef268470bc11aaf
Shreyas Narayan <shrena@codeaurora.org>
2020-03-11 18:13:07 +0530

    camera: disabled compilation
    Change-Id: I25dbbee2d973164c7390a7ae6ea03045db9eae49

--- Android.mk
+++ Android.mk
@@ -8,6 +8,6 @@ MM_V4L2_DRIVER_LIST += msm8909
 LOCAL_32_BIT_ONLY := true
 ifeq ($(call is-board-platform-in-list,$(MM_V4L2_DRIVER_LIST)),true)
     ifneq ($(BUILD_TINY_ANDROID),true)
-      include $(call all-subdir-makefiles)
+#      include $(call all-subdir-makefiles)
     endif
 endif
