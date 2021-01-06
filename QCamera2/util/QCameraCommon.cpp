b65d33109a4f79e64acecf4c0fcaba18d58f65fe
taojiang <taojiang@codeaurora.org>
2021-01-06 18:02:03 +0800

    QCamera2: HAL: add proper SOC_ID for sdm429 target
    Change-Id: Iad9124ef4d8d38a749abd44129ad19f6bc182da5

--- QCamera2/util/QCameraCommon.cpp
+++ QCamera2/util/QCameraCommon.cpp
@@ -1,4 +1,4 @@
-/* Copyright (c) 2016-2019, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2016-2019, 2020, The Linux Foundation. All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without
 * modification, are permitted provided that the following conditions are
@@ -379,7 +379,7 @@ bool QCameraCommon::is_target_SDM450()
 
 bool QCameraCommon::is_target_SDM429()
 {
-    return (parseHWID() == 354);
+    return (parseHWID() == 354 || parseHWID() == 416);
 }
 
 /*===========================================================================
